b1998da8b7a7fd2291a70863d5640089b11078dd
Nikreb35 <nikreb35+github@gmail.com>
2025-01-31 17:32:05 +0300

    CORS ayarları güncellendi

--- worker.js
+++ worker.js
@@ -109,13 +109,7 @@ async function handleAdminRequest(request, env) {
   if (request.method === 'OPTIONS') {
     return new Response(null, {
       status: 204,
-      headers: {
-        'Access-Control-Allow-Origin': 'https://tefas-cloudflare.pages.dev',
-        'Access-Control-Allow-Methods': 'GET, HEAD, POST, OPTIONS, PUT, DELETE',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization, target-url',
-        'Access-Control-Max-Age': '86400',
-        'Access-Control-Allow-Credentials': 'true'
-      }
+      headers: corsHeaders
     });
   }
 
